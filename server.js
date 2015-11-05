702ca517489306b79c0190660381cf1ae858eabd
QUINOESCOBAR <quinoescobar@gmail.com>
2015-11-05 01:27:35 +0000

    da error en head.ejs no se porque

--- server.js
+++ server.js
@@ -17,57 +17,25 @@ app.set('layout', 'layout');
 
 // Serve static files----------------------
 // http://expressjs.com/api.html#app.use#
-app.use(express.static('.'));
+app.use(express.static('static'));
 app.use(expressLayouts);
 
 // Luego la consultamos con app.get('port')
 app.set('port', (process.env.PORT || 8080));
 
-/*
-* body-parser is a piece of express middleware that
-* reads a form's input and stores it as a javascript
-* object accessible through `req.body`
-*
-* 'body-parser' must be installed (via `npm install --save body-parser`)
-* For more info see: https://github.com/expressjs/body-parser
-*/
-// instruct the app to use the `bodyParser()` middleware for all routes
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 // A browser's default method is 'GET', so this
 // is the route that express uses when we visit
 // our site initially.
 app.get('/', function(req, res){
-	// The form's action is '/' and its method is 'POST',
-	// so the `app.post('/', ...` route will receive the
-	// result of our form
-	res.render('body', { title: "form"});
+	res.render('pages/index.ejs', { title: "Index"});
 });
 
-
-// index page
-// app.get('/', function(req, res) {
-//     res.render('pages/index');
-// });
-
-// about page
-// app.get('/about', function(req, res) {
-//     res.render('pages/about');
-// });
-
-// This route receives the posted form.
-// As explained above, usage of 'body-parser' means
-// that `req.body` will be filled in with the form elements
-app.post('/', function(req, res){
-	var userName = req.body.userName;
-	res.render('greet', {userName: userName, title: 'greet'});
-});
 app.listen(8080);
 console.log('8080 es el puerto mágico :3');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 
 // var server = app.listen(8080, function () {
 //   var host = server.address().address;
